e7af7152115c44e685bb2bd183e89f1d6042afda
caldereth <135465775+caldereth@users.noreply.github.com>
2024-02-07 01:01:53 +1300

    rm IERC721Receiver

--- contracts/ERC404.sol
+++ contracts/ERC404.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC404} from "./IERC404.sol";
 import {ERC721Receiver} from "./ERC721Receiver.sol";
